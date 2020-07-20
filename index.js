4ff0ddec0e1c4a171dce49f38b591e958d5162b9
hnnav <henna.v@icloud.com>
2020-07-20 16:40:44 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
 // Your code here
+
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    
+}
+
+let createEmployeeRecords = function(data) {
+    return data.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(employee, dates){
+    let [date, hour] = dates.split(' ')
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
+
+let createTimeOutEvent = function(employee, dates){
+    let [date, hour] = dates.split(' ')
+
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
+
+let hoursWorkedOnDate = function(employee, theDates){
+    let inEvent = employee.timeInEvents.find(function(e){
+        return e.date === theDates
+    })
+
+    let outEvent = employee.timeOutEvents.find(function(e){
+        return e.date === theDates
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(employee, findDate){
+    let wage = hoursWorkedOnDate(employee, findDate)
+        * employee.payPerHour
+    return parseFloat(wage.toString())
+}
+
+let allWagesFor = function(employee){
+    let eligibleDates = employee.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate(employee, d)
+    }, 0)
+
+    return payable
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor(rec)
+    }, 0)
+}
